36e361849c94cf19eb12e8397e6317fe6a8b0655
dzehil02 <dzehil02@gmail.com>
2023-11-23 16:58:35 +0100

    Init commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    const hello = "Hello NodeJS !!! I'm developer !!!";
+    res.send(hello);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
